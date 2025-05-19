3c65eeb9b1d8888d6723c6db26296032fce38618
Dirk Sierd de Vries <dirksierd@me.com>
2025-05-19 19:44:08 +0200

    deps and aminpage

---
+++ app/pages/admin/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <AppPage>
+    <template #title>Beheer</template>
+    <AppBlock><p>Dit is een test.</p></AppBlock>
+  </AppPage>
+</template>
+
+<script setup lang="ts">
+  import AppBlock from '~/components/AppBlock.vue'
+  import AppPage from '~/components/AppPage.vue'
+</script>
